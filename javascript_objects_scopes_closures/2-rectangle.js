da42dc57a82731eb7cb22b8c792f8d12acec6d09
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 01:08:01 +0300

    add Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,14 @@
 that defines a rectangle:*/
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-        return {}; // Create an empty object
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object if w or h is not positive integers or equal to 0.
+        this.width = undefined;
+        this.height = undefined;
       }
-      this.width = w;
-      this.height = h;
     }
   }
   
