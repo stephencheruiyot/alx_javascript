546e79b16fd0443d02c9fd6b66d760e4fa907caa
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 00:47:50 +0300

    add rectangle 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+/*a class Rectangle 
+that defines a rectangle:*/
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height=h;
+      } else {
+        return{};
+      }
+        
+    }
+  }
+  
+  module.exports = Rectangle;
+  
