da9f251f1a2958f1883651d7c610bbbd5d92ac36
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 11:22:42 +0300

    add require

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,30 +1,4 @@
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-        return {};
-      }
-      this.width = w;
-      this.height = h;
-    }
-  
-    print() {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    rotate() {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  
-    double() {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-  
+const Rectangle = require('./4-rectangle');
   class Square extends Rectangle {
     constructor(size) {
       super(size, size);
