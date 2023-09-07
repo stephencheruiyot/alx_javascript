780621b4d8ce56dd9b7189b90e56033b964d2dd4
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 10:00:23 +0300

    add null

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -5,8 +5,12 @@ class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
         // If w or h is not a positive integer or equal to 0, create an empty object
-        return {};
-        console.log(Rectangle);
+        return {
+          width : null ,
+          height :null
+          
+        };
+        
       }
       
       this.width = w;
