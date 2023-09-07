768863e8cf209f86e89af10f9a9d0420c9673c8d
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 11:09:07 +0300

    del example

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -32,21 +32,4 @@ class Rectangle {
   }
   
   module.exports = { Rectangle, Square };
-  
-  // Example usage:
-  const r1 = new Rectangle(2, 3);
-  console.log('Normal:');
-  r1.print();
-  
-  console.log('Double:');
-  r1.double();
-  r1.print();
-  
-  console.log('Rotate:');
-  r1.rotate();
-  r1.print();
-  
-  const s1 = new Square(4);
-  console.log('Square:');
-  s1.print();
-  
+    
