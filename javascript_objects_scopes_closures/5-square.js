55dc2a3efb3b6ea36e3b1cdee864bb095022dfe6
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 11:33:44 +0300

    add 5 square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,4 +1,4 @@
-const Rectangle = require('./4-rectangle');
+const derivedRectangle = require('./4-rectangle');
   class Square extends Rectangle {
     constructor(size) {
       super(size, size);
