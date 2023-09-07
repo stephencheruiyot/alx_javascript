86f284d8ac25154bcceaf6793ddb1951ba395d29
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-07 10:16:05 +0300

    add else

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -5,10 +5,10 @@ class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
         // If w or h is not a positive integer or equal to 0, create an empty object
-      return Rectangle;
-          
-      }
-      
+        this.width = {};
+        this.height = {};
+        } else{
+                     
       this.width = w;
       this.height = h;
     }
