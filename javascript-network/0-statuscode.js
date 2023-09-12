129baa0763c457a1a01c715b09fec3154451f4b0
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-12 18:14:45 +0300

    add shebang

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const request = require('request');
 request('https://intranet.alxswe.com', function (error, response) {
     console.log('Code:', response && response.statusCode);
