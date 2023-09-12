aba442c6d024dd188226e3430fcdd65db0e6acc2
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-12 18:30:02 +0300

    catch error

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 const request = require('request');
-request('https://intranet.alxswe.com', function (error, response) {
-    console.log('Code:', response && response.statusCode);
+
+// Define the URL you want to request
+const urlToRequest = 'https://intranet.alxswe.com';
+
+// Make the GET request
+request('urlToRequest', function (error, response) {
+    if (error){
+        console.log('Error: ${error.message}');
+    } else {
+        // Display the status code
+    console.log('Code:, ${response.statusCode}');
+    }
 });
