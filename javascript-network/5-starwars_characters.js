ddfec09e458471d957d34c21242a148c9815188d
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-14 19:28:26 +0300

    add 5 starw

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,46 +1,46 @@
 const request = require('request');
 
-// Check if a movie ID is provided as a command-line argument
-if (process.argv.length < 3) {
-  console.error('Usage: node 5-starwars_characters.js <Movie ID>');
+// Get the Movie ID from command-line arguments
+const movieId = process.argv[2];
+
+if (!movieId || isNaN(movieId)) {
+  console.log('Please provide a valid Movie ID as the first argument.');
   process.exit(1);
 }
 
-// Extract the movie ID from the command-line argument
-const movieId = process.argv[2];
-
-// URL to fetch movie details from SWAPI
+// Define the URL to fetch movie details from SWAPI
 const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-// Function to fetch and print characters from a movie
-function printCharacters(movieUrl) {
-  request(movieUrl, (error, response, body) => {
+// Function to fetch characters for a given movie
+function fetchCharactersForMovie(url) {
+  request(url, (error, response, body) => {
     if (error) {
-      console.error('Error fetching movie details:', error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Failed to fetch movie details. Status code:', response.statusCode);
-      return;
-    }
-
-    const movie = JSON.parse(body);
-    console.log(`Characters in "${movie.title}":`);
-    movie.characters.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error fetching character details:', charError);
-        } else if (charResponse.statusCode === 200) {
-          const character = JSON.parse(charBody);
-          console.log(character.name);
-        } else {
-          console.error('Failed to fetch character details. Status code:', charResponse.statusCode);
-        }
+      console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+      console.error(`HTTP Error: ${response.statusCode}`);
+    } else {
+      const movieData = JSON.parse(body);
+      console.log(`Characters in "${movieData.title}":`);
+      movieData.characters.forEach((characterUrl) => {
+        fetchCharacterName(characterUrl);
       });
-    });
+    }
   });
 }
 
-// Start fetching and printing characters
-printCharacters(movieUrl);
+// Function to fetch and print character names
+function fetchCharacterName(url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+      console.error(`HTTP Error: ${response.statusCode}`);
+    } else {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    }
+  });
+}
+
+// Fetch characters for the specified movie
+fetchCharactersForMovie(movieUrl);
