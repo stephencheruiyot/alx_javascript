f4086b851b5689f77edf235d79d9d67b95607b0a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-14 18:51:35 +0300

    remove url

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -16,7 +16,7 @@ request(apiUrl, (error, response, body) => {
 
   const films = JSON.parse(body).results;
   const numberOfMoviesWithWedgeAntilles = films.reduce((count, film) => {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+    if (film.characters.includes(`${characterId}`)) {
       count++;
     }
     return count;
