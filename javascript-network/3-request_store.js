969664abc17575fb43ac19b201a15ca003ad17ad
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-14 16:40:28 +0300

    add 3 request

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,24 +1,35 @@
-#!/usr/bin/env node
 const request = require('request');
 const fs = require('fs');
+const utf8 = 'utf-8';
+
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node fetchAndSave.js <URL> <filePath>');
+  process.exit(1);
+}
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Make the HTTP request to fetch the webpage contents
-request(url, (error, response, body) => {
+// Make an HTTP request to the provided URL
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-  } else {
-    const content = body.trim(); // Trim whitespace
-    fs.writeFile(filePath, content, 'utf8', (err) => {
+    console.error('Error requesting the URL:', error);
+    process.exit(1);
+  }
+
+  // Check if the request was successful (status code 200)
+  if (response.statusCode === 200) {
+    // Write the response body to a file
+    fs.writeFile(filePath, body, { encoding: utf8 }, (err) => {
       if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log;
+        console.error('Error writing to the file:', err);
+        process.exit(1);
       }
+      console.log(`Webpage content saved to ${filePath} successfully.`);
     });
+  } else {
+    console.error(`Failed to fetch the URL. Status code: ${response.statusCode}`);
+    process.exit(1);
   }
 });
