4b6dd94a6d4a82e6a0c3f77028a799b0d799919e
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-14 17:36:22 +0300

    add api

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/'
+
 // Make a GET request to the Star Wars API
 request(apiUrl, (error, response, body) => {
   if (error) {
