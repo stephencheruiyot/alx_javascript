129118d9e8b57b93afcd3c236ffce1288aadfe05
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-09-14 17:33:37 +0300

    remove comand line args

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,14 +1,5 @@
 const request = require('request');
 
-// Check if the API URL is provided as a command line argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-// Get the API URL from the command line argument
-const apiUrl = process.argv[2];
-
 // Make a GET request to the Star Wars API
 request(apiUrl, (error, response, body) => {
   if (error) {
